69a7a90583bf0710aea59b201e777de96c44a0ae
MeViPortal <95439264+MeViPortal@users.noreply.github.com>
2021-12-16 20:44:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,25 +3,60 @@
 sitemap:
 exclude: 'yes'
 ---
+$('.post-list').load('/index.html #post-list-blog')
 
+//news
   $(document).ready(function () {
     {% if site.disable_landing_page != true %}
-    $('a.blog-button').click(function (e) {
-      if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    let opened = false;
+    $('a.news-button').click(function (e) {
+      if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
+        $('.post-list').load('/index.html #post-list-news')
+
+      }
       currentWidth = $('.panel-cover').width()
       if (currentWidth < 960) {
         $('.panel-cover').addClass('panel-cover--collapsed')
+        $('.post-list').load('/index.html #post-list-news')
         $('.content-wrapper').addClass('animated slideInRight')
       } else {
         $('.panel-cover').css('max-width', currentWidth)
-        $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
+        $('.post-list').load('/index.html #post-list-news')
+        $('.content-wrapper').addClass('animated slideInRight')
+        $('.panel-cover').animate({ 'max-width': '450px', 'width': '33%' }, 400, swing = 'swing', function () { })
       }
     })
 
-    if (window.location.hash && window.location.hash == '#blog') {
+    if (window.location.hash && window.location.hash == '#news') {
+      $('.post-list').load('/index.html #post-list-news')
       $('.panel-cover').addClass('panel-cover--collapsed')
     }
+// games
+    $('a.games-button').click(function (e) {
+      if ($('.panel-cover').hasClass('panel-cover--collapsed')){
+        $('.post-list').load('/index.html #post-list-games')
 
+      } 
+      currentWidth = $('.panel-cover').width()
+      if (currentWidth < 960) {
+        $('.panel-cover').addClass('panel-cover--collapsed')
+        $('.post-list').load('/index.html #post-list-games')
+        $('.content-wrapper').addClass('animated slideInRight')
+      } else {
+        $('.panel-cover').css('max-width', currentWidth)
+        $('.post-list').load('/index.html #post-list-games')
+        $('.content-wrapper').addClass('animated slideInRight')
+        $('.panel-cover').animate({'max-width': '450px', 'width': '33%'}, 400, swing = 'swing', function () {})
+      }
+    })
+  
+  if (window.location.hash && window.location.hash == '#games') {
+      $('.post-list').load('/index.html #post-list-games')
+      $('.panel-cover').addClass('panel-cover--collapsed')
+    }
+     
+     
+     
     if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
       $('.panel-cover').addClass('panel-cover--collapsed')
     }
@@ -32,7 +67,12 @@ exclude: 'yes'
       $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
     })
 
-    $('.navigation-wrapper .blog-button').click(function () {
+    $('.navigation-wrapper .news-button').click(function () {
+      $('.navigation-wrapper').toggleClass('visible')
+      $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    })
+
+    $('.navigation-wrapper .games-button').click(function () {
       $('.navigation-wrapper').toggleClass('visible')
       $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
     })
